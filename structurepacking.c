ad5b7b519912af5e3f3e32c45d36c52ec7fd7ca7
venki <venkateshvenki77644@gmail.com>
2023-11-10 19:13:12 +0530

    initial commit

---
+++ structurepacking.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#pragma pack(1)
+
+#pragma pack(1)
+
+struct stu
+{
+    char a;
+    char b;
+    int c;
+} s;
+
+int main()
+{
+    printf("Size Of Structure Is: %d", sizeof(s));
+}
