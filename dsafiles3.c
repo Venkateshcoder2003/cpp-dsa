a5bf8889ca4974ee5f8dabe5e00829430c2f97c3
venki <venkateshvenki77644@gmail.com>
2023-11-23 15:51:06 +0530

    writing string into file using fputs

---
+++ dsafiles3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    FILE *fp;
+    char str[20];
+    fp = fopen("file3.txt", "w");
+    printf("Enter a String: \n");
+    scanf("%s", str);
+    if (fp == NULL)
+    {
+        printf("error in Opening a file");
+        return 1;
+    }
+    for (int i = 0; i < strlen(str); i++)
+    {
+        fputc(str[i], fp);
+    }
+}
