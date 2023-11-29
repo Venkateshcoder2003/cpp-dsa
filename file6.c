38a00a0c89c82d63dafcd0ff2b5b6bc959e4cc9e
venki <venkateshvenki77644@gmail.com>
2023-11-29 20:48:46 +0530

    counting number of lines in file

---
+++ file6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *fp;
+    char ch;
+    fp = fopen("abc.txt", "r");
+    if (fp == NULL)
+    {
+        printf("\nError In Opening a File");
+        exit(0);
+    }
+    int count = 1;
+    while ((ch = fgetc(fp)) != EOF)
+    {
+        if (ch == '\n')
+        {
+            count++;
+        }
+    }
+    fclose(fp);
+    printf("Number Of Lines:%d \n", count - 1);
+    return 0;
+}
