dfb5fb7ff4cdf24b95932eb38100668d551b6b36
venki <venkateshvenki77644@gmail.com>
2023-12-31 15:28:01 +0530

    tree recursion

---
+++ recursion3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int fun(int n)
+{
+    if (n > 0)
+    {
+        printf("%d\t", n);
+        fun(n - 1);
+        fun(n - 1);
+        // fun(n-1);
+    }
+}
+
+int main()
+{
+    int a = 3;
+    fun(a);
+    printf("\n");
+    printf("%d", fun(a));
+    return 0;
+}
