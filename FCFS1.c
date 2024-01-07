a4f4b6d52c8ad4b3b197714f0ced035aeeccd6c4
venki <venkateshvenki77644@gmail.com>
2024-01-07 07:37:28 +0530

    fcfs scheduling algorithm

--- FCFS1.c
+++ FCFS1.c
@@ -1,47 +1,69 @@
 #include <stdio.h>
 
+struct process
+{
+    int at;
+    int bt;
+    int wt;
+    int tat;
+    int ct;
+    int temp;
+};
+
 int main()
 {
     int n;
     printf("Enter the number of processes: ");
     scanf("%d", &n);
 
-    int at[n], bt[n], wt[n], tat[n], ct[n], temp[n];
+    struct process p[n];
 
     float awt = 0, atat = 0;
 
     for (int i = 0; i < n; i++)
     {
         printf("Enter Burst Time Of Process %d: ", i + 1);
-        scanf("%d", &bt[i]);
+        scanf("%d", &p[i].bt);
     }
     for (int i = 0; i < n; i++)
     {
         printf("Enter Arrival Time Of Process %d: ", i + 1);
-        scanf("%d", &at[i]);
+        scanf("%d", &p[i].at);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (p[j].at > p[j + 1].at)
+            {
+                struct process temp1 = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp1;
+            }
+        }
     }
 
-    temp[0] = 0;
+    p[0].temp = 0;
     printf("PROCESS\t\tBURST TIME\tARRIVAL TIME\tWAITING TIME\tTURN AROUND TIME\tCOMPLETION TIME\n");
     for (int i = 0; i < n; i++)
     {
-        wt[i] = 0;
-        tat[i] = 0;
+        p[i].wt = 0;
+        p[i].tat = 0;
 
         // Calculate waiting time, turn-around time, and completion time
         if (i > 0)
         {
-            temp[i] = temp[i - 1] + bt[i - 1];
-            wt[i] = temp[i] - at[i];
+            p[i].temp = p[i - 1].temp + p[i - 1].bt;
+            p[i].wt = p[i].temp - p[i].at;
         }
 
-        tat[i] = wt[i] + bt[i];
-        ct[i] = tat[i] + at[i];
+        p[i].tat = p[i].wt + p[i].bt;
+        p[i].ct = p[i].tat + p[i].at;
 
-        awt += wt[i];
-        atat += tat[i];
+        awt += p[i].wt;
+        atat += p[i].tat;
 
-        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t\t%d\n", i + 1, bt[i], at[i], wt[i], tat[i], ct[i]);
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t\t%d\n", i + 1, p[i].bt, p[i].at, p[i].wt, p[i].tat, p[i].ct);
     }
 
     awt = awt / n;
